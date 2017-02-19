7b8ce57563211f32e2259f263ad5e6f276a4c890
sab0991 <sganapathy3@wisc.edu>
2017-02-19 11:45:57 -0600

    Scripts for gen positive and negative fys , training data

---
+++ gen_neg_fys.py
@@ -0,0 +1,109 @@
+""" Take a text document
+1) Ignore words in the adj tag
+2) Prune the negative examples
+	- those that are atleast 4 chars long and have atleast one of "preceding chars" features set
+"""
+
+
+import re
+from sets import Set
+
+
+def GenNegFys( list_of_words,target_num ):
+
+	features = []
+	negative_words = []
+
+	tag1    = "<adj>"
+	tag2	= "</adj>"
+	tag_set = Set( [tag1,tag2] )
+ 
+	third_features = Set( ['was','a','are','is','so','were'] )
+	
+	num_negative = 0
+
+	for windx in range( len(list_of_words) ):
+
+		prev_word_indx = windx - 1
+
+		if prev_word_indx >=0  and list_of_words[windx] not in tag_set and tag1 not in list_of_words[windx] and tag2 not in list_of_words[windx] :
+
+
+			""" Feature - len in chars """
+
+			fys_list = []
+
+			fys_list.append ( len( list_of_words[windx] ) )
+
+
+			""" Feature-Preceded by Super """
+		
+			prev_word_indx_2 = prev_word_indx-1
+
+			if prev_word_indx_2 > 0 :
+
+				if  list_of_words[ prev_word_indx_2 ].lower() == "super" :
+			
+					fys_list.append( int(1) )
+				else:
+					fys_list.append( int(0) )
+
+			else:
+
+				fys_list.append( int(0) )
+		
+			""" Feature- third and fourth """
+
+			prev_word = list_of_words[ prev_word_indx ]
+		
+			if prev_word.lower() in third_features :
+
+				fys_list.append( int(1) )
+			else:
+			
+				fys_list.append( int(0) )
+
+			if prev_word.lower() == "very":
+				fys_list.append( int(1) )
+			else:
+			
+				fys_list.append( int(0) )
+
+
+			""" Pruning examples"""
+
+			if  fys_list[0] >=4 and fys_list[1] + fys_list[2] + fys_list[3] >= 1 :
+
+				features.append( fys_list )
+				negative_words.append( list_of_words[windx] )
+				num_negative+=1
+
+			
+
+	if num_negative < target_num:
+	
+		negative_word_set = Set( negative_words )
+		
+		for windx in range( len(list_of_words) ):
+
+			prev_word_indx = windx - 1
+
+			if num_negative >= target_num:
+				break
+
+
+			if prev_word_indx >=0  and list_of_words[windx] not in tag_set and list_of_words[windx] not in negative_word_set and tag1 not in list_of_words[windx] and tag2 not in list_of_words[windx]:
+
+			
+				if ( len ( list_of_words[windx] ) >= 4 ):
+					fys_list = [len(list_of_words),0,0,0]
+					features.append ( fys_list )
+					negative_words.append( list_of_words[windx] )
+					num_negative+=1
+				
+
+		
+
+
+
+	return features,negative_words
--- gen_pos_fys.py
+++ gen_pos_fys.py
@@ -4,82 +4,181 @@
 """
 
 import re
+import os
+import numpy
 from sets import Set
+from collections import defaultdict
+from gen_neg_fys import GenNegFys
 
 
-text_file = open('Data/text/review48.txt','r')
+""" An adjective is only one word long """
 
-""" An adjective is only one or two words long """
+p = re.compile("<adj> (\w+) </adj>")
 
-p = re.compile("<adj> (\w+\s*\w*) </adj>")
-
+training_words = []
 features = []
+feature_names = []
+target_label = []
 
-features.append ( [] )
-features[0].append("Length in words")
-features[0].append("Contains super")
-features[0].append("Is preceded by was/a/are/is/so")
-features[0].append("Is positive example")
+feature_names.append("Length in chars")
+feature_names.append("Is preceded by super")
+feature_names.append("Is preceded by was/a/are/is/were/so")
+feature_names.append("Is preceded by very")
+feature_names.append("Is positive example")
+
 
 list_indx = 0
 
-third_features = Set( ['was','a','are','is','so'] )
+third_features = Set( ['was','a','are','is','so','were'] )
 
-for line in text_file:
+directory = "/home/sabareesh/DataScience/DataScience-Foodie/Data/TaggedFiles/"
+
+it = 0
+
+for filename in os.listdir(directory):
 		
-    adj_list = p.findall( line )
-    
-    list_of_words = line.split()
 
-    for adj in adj_list:
+	filepath = directory + filename
 
-	 features.append( [] )
-	 list_indx+=1
+	with open(filepath,'r') as myFile:
+		data=myFile.read().replace('\n','')
 	
-	 """Length Feature"""
-
-	 features[ list_indx ].append( len ( adj.split() ) )
-	 
 	
-	 """Second Feature"""
-
-	 first_word_adj = adj.split()[0]
-
-	 if  first_word_adj.lower() == "super" : 
+	""" Same adjective may appear multiple times with
+	    different features thus a default dictionary 
+	    is created with key=word and value=tuple consisting of
+	    (list_indx,visited_flag). Thus a key will point to a list
+	    if multiple occurences are present """
 	
-	 	features[ list_indx ].append( int(1) )
+	
 
-	 else:
-	 	features[ list_indx ].append( int(0) )
-	 
-        
-         """ Finding the prev word ( before tag ) for rest of features  """
+	list_of_words = data.split()
+	
+	index_list = list ( range(0,len(list_of_words) ) )
+	flag_list = [0]*len(list_of_words)
+	
+	index_flag_list = zip(index_list,flag_list)
+	
+	word_dict = defaultdict( list )
+	
+	for i in range ( len (list_of_words) ):
+		word_dict[ list_of_words[i] ].append( index_flag_list[i] )
+	
+	adj_list = p.findall( data )
 
-	 prev_word_indx =  list_of_words.index(first_word_adj) - 2
-	 prev_word = ""
-
-	 if   prev_word_indx >= 0 : 
+	positive_words = []
+	
+	for adj in adj_list:
+	
+	
+		if len(adj) >= 4:
 		
-	 	prev_word = list_of_words[ prev_word_indx] 
-
-	 
-	 """Third Feature"""
+			positive_words.append( adj )
 	
-	 if  prev_word.lower() in third_features :
-
-	 	features[list_indx].append ( int(1) )
-
-	 else :
+			""" Length """
+		
+			features.append( [] )
 	
-	 	features[ list_indx].append( int(0) )
-
+			features[list_indx].append( len( adj ) )
 	
-         """Fourth feature"""
+	
+	       		""" Finding index,visited list from dictionary """
+	
+			cur_indx = list_of_words.index( adj )
+	
+			indx_visited_list = word_dict[ adj ]
+	
+		
+			for i in range( len(indx_visited_list) ):
+			
+		    		if indx_visited_list[i][1] == 0:
+	
+					cur_indx = indx_visited_list[i][0]
+					indx_visited_list[i] = (cur_indx,1)
+					break
+	
+		
+			""" Updating word_dict with visited information """
+	
+			word_dict[ adj ] = indx_visited_list
+	
+			prev_word_indx =  cur_indx - 2
+			prev_word_indx_2 = prev_word_indx-1
+			prev_word = ""
+			prev_word_2 = ""
+	
+	
+	        	""" Preceded by super """
+	
+	
+			if   prev_word_indx_2 >= 0 : 
+				
+				prev_word_2 = list_of_words[ prev_word_indx_2 ]
+	
+				if prev_word_2.lower() == "super":
+	
+					features[list_indx].append( int(1) )
+	
+				else:
+					features[list_indx].append( int(0) )
+	
+	
+			else:
+	
+				features[ list_indx ].append( int(0) )
+	
+			 
+			if   prev_word_indx >=0 :
+	
+				prev_word = list_of_words[ prev_word_indx]
+	
+	
+			if  prev_word.lower() in third_features :
+				
+				features[list_indx].append(int(1))
+	
+			else: 
+				features[list_indx].append( int(0) )
+	
+	
+			""" Preceded by very """
+	
+			if prev_word.lower() == "very" :
+	
+				features[list_indx].append( int(1) )
+	
+			else:
+				features[list_indx].append( int(0) )
+	
+			
+			list_indx+=1
 
-	 features[list_indx].append( int(1) )
+			
+				
+	target_num     = len(positive_words) + 8 
+	[negative_fys,negative_words] = GenNegFys( list_of_words, target_num )
+	features       = features + negative_fys
+	list_indx      = list_indx + len( negative_fys ) 
+	target_label   = target_label + [1]*len(positive_words) + [0]*len(negative_words)
+	training_words =  training_words + positive_words + negative_words
+		
 
 
 
+print target_label.count(1)
+print target_label.count(0)
 
-print features 
-	 
+numpy.save('features.npy',features) 
+numpy.savetxt('features.txt',features)
+
+numpy.save('target_label.npy',target_label)
+numpy.savetxt('target_label.txt',target_label)
+
+
+numpy.savetxt('training_words.txt',training_words,fmt='%s')
+
+numpy.savetxt('feature_names.txt',feature_names,fmt='%s')	 
+
+numpy.save('training_words.npy',training_words)
+
+numpy.save('feature_names.npy',feature_names)
