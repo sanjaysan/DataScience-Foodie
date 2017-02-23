9f744b6288a027b2dd98f0d0713d23c770050596
sanjaysesh <sanjay93@cs.wisc.edu>
2017-02-22 21:25:05 -0600

    New directory for all the classifiers

---
+++ Classifiers/LinearRegression.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn import linear_model
+
+features = np.load('Data/Training/features.npy')
+labels   = np.load('Data/Training/target_label.npy')
+
+reg = linear_model.LinearRegression()
+
