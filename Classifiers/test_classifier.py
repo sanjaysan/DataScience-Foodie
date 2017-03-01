b6e53994931f1edc1f85c2892107a2f5a8e2bf1f
sanjaysesh <sanjay93@cs.wisc.edu>
2017-02-28 22:23:30 -0600

    Classifier for test data

--- Classifiers/test_classifier.py
+++ Classifiers/test_classifier.py
@@ -1,21 +1,24 @@
 import numpy as np
 from sklearn import svm
+from sklearn import linear_model
 
-features = np.load('Data/Training/features.npy')
-labels = np.load('Data/Training/target_label.npy')
-training_words = np.load('Data/Training/training_words.npy')
+features = np.load('../Data/Training/features.npy')
+labels = np.load('../Data/Training/target_label.npy')
+training_words = np.load('../Data/Training/training_words.npy')
 
-clf_test = svm.SVC()
+# clf_test = svm.SVC()
+clf_test = linear_model.LinearRegression()
 clf_test = clf_test.fit(features, labels)
 
-test_features = np.load('Data/Testing/test_features.npy')
-test_labels = np.load('Data/Testing/test_target_label.npy')
-test_words = np.load('Data/Testing/testing_words.npy')
+test_features = np.load('../Data/Testing/test_features.npy')
+test_labels = np.load('../Data/Testing/test_target_label.npy')
+test_words = np.load('../Data/Testing/testing_words.npy')
 
-prediction_score = clf_test.decision_function(test_features)
-min_threshold = 0.8
+# prediction_score = clf_test.decision_function(test_features)
+prediction_label = clf_test.predict(test_features)
+min_threshold = 0.63
 
-prediction_label = [1 if y_s > min_threshold else 0 for y_s in prediction_score]
+prediction_label = [1 if y_s > min_threshold else 0 for y_s in prediction_label]
 
 num_pos_predictions = np.sum(prediction_label)
 
