c6e2109e2bbe7003018ed9f39d10c18abf50af1c
sanjaysan <sanjaysan01@gmail.com>
2017-05-05 22:13:02 -0500

    1) Added a few missing city values in match_data.csv in DataMerging folder 2) Changed the data extraction from the merged table to extract city names separately. 3) Changed model.json to remove the issue with rating attribute 4) Wrote OLAP queries to drill down on location 5) Generated restaurant_details.csv in the OLAPExploration folder using the city extraction logic

--- OLAPExploration/aggregate.py
+++ OLAPExploration/aggregate.py
@@ -1,20 +1,55 @@
 from __future__ import print_function
 from cubes import Workspace, Cell, PointCut
+import warnings
 
-# 1. Create a workspace
+warnings.filterwarnings('ignore')
+
+# 1. Creating a workspace
 workspace = Workspace()
 workspace.register_default_store("sql", url="sqlite:///restaurant.sqlite")
 workspace.import_model("model.json")
 
-# 2. Get a browser
-browser = workspace.browser("irbd_balance")
+# 2. Getting a browser
+browser = workspace.browser("restaurant_details")
 
-# 3. Play with aggregates
+# 3. Aggregating the results
 result = browser.aggregate()
 
-print("Total\n"
-      "----------------------")
+print("Total\n-------------------------------")
 
-print("Record count : %8d" % result.summary["record_count"])
-print("Total amount : %8d" % result.summary["amount_sum"])
-print("Double amount: %8d" % result.summary["double_amount_sum"])
+print("Price Range sum : %8d" % result.summary["price_range_sum"])
+print("Rating Value average : %8d" % result.summary["rating_average"])
+
+# Drilling down by Location
+print("\n"
+      "Drill Down by Location\n"
+      "======================")
+
+result = browser.aggregate(drilldown=["location"])
+browser.aggregate()
+
+print(("%-70s%4s%8s\n" + "-" * 84) % ("Street", "Price", "Rating"))
+for row in result.table_rows("location"):
+    print("%-70s%4d%8d" % (row.label,
+                           row.record["price_range_sum"],
+                           row.record["rating_average"])
+          )
+
+print("\n"
+      "Slice where Location = WA\n"
+      "==================================================")
+
+cut = [
+    PointCut("location", ["WA"]),
+    PointCut("location", ["TX"])
+]
+
+cell = Cell(browser.cube, cuts=cut)
+result = browser.aggregate(cell, drilldown=["location"])
+print(("%-20s%10s\n" + "-" * 50) % ("State", "Zipcode"))
+
+for row in result.table_rows("location"):
+    print("%-20s%4d%8d" % (row.label,
+                           row.record["price_range_sum"],
+                           row.record["rating_average"]
+                           ))
