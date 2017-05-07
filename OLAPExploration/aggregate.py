8c25172c8c0f2364205dacc0982e64efa62d3d7e
sanjaysan <sanjaysan01@gmail.com>
2017-05-07 01:41:58 -0500

    Wrote logic to rollup to states using cubes framework

--- OLAPExploration/aggregate.py
+++ OLAPExploration/aggregate.py
@@ -3,6 +3,40 @@ from cubes import Workspace, Cell, PointCut
 import warnings
 
 warnings.filterwarnings('ignore')
+count = 1
+
+def rollup(cell, dimension):
+    global count
+    """Drill-down and aggregate recursively through als levels of `dimension`.
+
+    This function is like recursively traversing directories on a file system
+    and aggregating the file sizes, for example.
+
+    * `cell` - cube cell to drill-down
+    * `dimension` - dimension to be traversed through all levels
+    """
+
+
+    result = browser.aggregate(cell, drilldown=[dimension])
+
+    # for row in cubes.drilldown_rows(cell, result, dimension):
+    for row in result.table_rows(dimension):
+        #indent = "    " * (len(row.path) - 1)
+        print (row.label)
+        print ("-" * 2)
+        print ("Total Number of reviews: %d" % (row.record["total_number_of_reviews"]))
+        print("Price average: %d" % float(row.record["price_average"]))
+        print("Rating average: %d" % float(row.record["rating_average"]))
+        print ("\n")
+
+        # new_cell = cell.drilldown(dimension, row.key)
+        # if new_cell is not None:
+        #     count += 1
+        #     if (count >= 2):
+        #         count = 1
+        #         break
+
+        # drilldown(cell, dimension)
 
 # 1. Creating a workspace
 workspace = Workspace()
@@ -13,51 +47,53 @@ workspace.import_model("model.json")
 browser = workspace.browser("restaurant_details")
 
 # 3. Aggregating the results
-result = browser.aggregate()
+# result = browser.aggregate()
 
-print("Total\n-------------------------------")
-
-print("Total number of reviews: %8d" % result.summary["total_number_of_reviews"])
-print("Price Average : %8d" % result.summary["price_average"])
-print("Rating Value average : %8d" % result.summary["rating_average"])
+# print("Total\n-------------------------------")
+#
+# print("Total number of reviews: %8d" % result.summary["total_number_of_reviews"])
+# print("Price Average : %8d" % result.summary["price_average"])
+# print("Rating Value average : %8d" % result.summary["rating_average"])
 
 # Drilling down by Location
 print("\n"
       "Drill Down by Location\n"
       "======================")
 
-
-result = browser.aggregate(drilldown=["location:state"])
-
-result = browser.aggregate(drilldown=["location:state"])
-
-for row in result.table_rows("location"):
-    print("%-70s%4d%8d%8d" % (row.label,
-			   row.record["total_number_of_reviews"],
-                           row.record["price_average"],
-			   row.record["rating_average"])
-
-	 )
+cell = Cell(browser.cube)
+rollup(cell, "location")
 
 
-
-
-print("\n"
-      "Slice where Location = CA\n"
-      "==================================================")
-
-cut = PointCut("location", ["CA"])
-cell = Cell(browser.cube,[cut])
-result = browser.aggregate(cell,drilldown=["location.state"])
-
-print(("%-20s%4s%4s%4s\n" + "-" * 84) % ("State","Number_of_reviews", "Price", "Rating"))
-
-for row in result:
-    print ( "%-20s%4d%4d%4d" % (row["location.state"], row["total_number_of_reviews"],row["price_average"],row["rating_average"]) )
-
-
-
-
+# result = browser.aggregate(cell, drilldown=["location"])
+#
+#
+# for row in result.table_rows("location"):
+#     print("%-70s%4d%8d%8d" % (row.label,
+# 			   row.record["total_number_of_reviews"],
+#                            row.record["price_average"],
+# 			   row.record["rating_average"])
+# 	 )
+#     new_cell = cell.drilldown("location", row.key)
+#
+#
+#
+#
+# print("\n"
+#       "Slice where Location = CA\n"
+#       "==================================================")
+#
+# cut = PointCut("location", ["CA"])
+# cell = Cell(browser.cube,[cut])
+# result = browser.aggregate(cell,drilldown=["location.state"])
+#
+# print(("%-20s%4s%4s%4s\n" + "-" * 84) % ("State","Number_of_reviews", "Price", "Rating"))
+#
+# for row in result:
+#     print ( "%-20s%4d%4d%4d" % (row["location.state"], row["total_number_of_reviews"],row["price_average"],row["rating_average"]) )
+#
+#
+#
+#
 
 
 
